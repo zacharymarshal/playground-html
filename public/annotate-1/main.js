c8660c082a173b6c72bc463dc1025d163b68a81e
Zachary Rankin <zach@rknmail.com>
2022-12-22 23:16:29 +0000

    Allow for adding multiple objects

--- public/annotate-1/main.js
+++ public/annotate-1/main.js
@@ -1,55 +1,8 @@
-const { createElement: e, Fragment, useEffect, useState } = React;
+const { createElement: e, Fragment, useEffect, useState } = window.React;
 
-function Annotate() {
-  const [isOn, setIsOn] = useState(false);
-  const [posX, setPosX] = useState(0);
-  const [posY, setPosY] = useState(0);
-  const [coords, setCoords] = useState([]);
-  const [width, setWidth] = useState(50);
-  const [height, setHeight] = useState(50);
+const DEFAULT_LENGTH = 50;
 
-  useEffect(() => {
-    if (!isOn) {
-      return;
-    }
-
-    let isDrawingYup = false;
-    let posXYup = 0;
-    let posYYup = 0;
-    const d = (e) => {
-      e.preventDefault();
-      console.log("mousedown", e);
-      isDrawingYup = true;
-      setPosX(e.pageX - width / 2);
-      posXYup = e.pageX - width / 2;
-      setPosY(e.pageY - height / 2);
-      posYYup = e.pageY - height / 2;
-      setCoords([[width / 2, height / 2]]);
-    };
-    window.addEventListener("mousedown", d);
-
-    const u = () => {
-      isDrawingYup = false;
-      console.log("mouseup");
-    };
-    window.addEventListener("mouseup", u);
-
-    const m = (e) => {
-      if (!isDrawingYup) {
-        return;
-      }
-      console.log({ posXYup, x: e.pageX });
-      setCoords((c) => [...c, [e.pageX - posXYup, e.pageY - posYYup]]);
-    };
-    window.addEventListener("mousemove", m);
-
-    return () => {
-      window.removeEventListener("mousedown", d);
-      window.removeEventListener("mouseup", u);
-      window.removeEventListener("mousemove", m);
-    };
-  }, [isOn]);
-
+function Layer({ coords, posX, posY }) {
   const [mx, my] = coords[0] || [0, 0];
   const pathStr =
     `M${mx} ${my} ` +
@@ -66,13 +19,13 @@ function Annotate() {
 
   let minX = coords.reduce((acc, [cx, cy]) => (cx < acc ? cx : acc), 0) - 5;
   let maxX = coords.reduce((acc, [cx, cy]) => (cx > acc ? cx : acc), 0) + 5;
-  const twidth = Math.max(width, maxX) + Math.abs(minX);
+  const twidth = Math.max(DEFAULT_LENGTH, maxX) + Math.abs(minX);
 
   let minY = coords.reduce((acc, [cx, cy]) => (cy < acc ? cy : acc), 0) - 5;
   let maxY = coords.reduce((acc, [cx, cy]) => (cy > acc ? cy : acc), 0) + 5;
-  const theight = Math.max(height, maxY) + Math.abs(minY);
+  const theight = Math.max(DEFAULT_LENGTH, maxY) + Math.abs(minY);
 
-  const svg = e(
+  return e(
     "svg",
     {
       xmlns: "http://www.w3.org/2000/svg",
@@ -87,29 +40,101 @@ function Annotate() {
     },
     path
   );
+}
 
-  const onBtn = e(
-    "button",
-    {
-      onClick: () => setIsOn(true),
-    },
-    "ON"
-  );
+function DrawTool({ onDone }) {
+  const [isDrawing, setIsDrawing] = useState(false);
+  const [posX, setPosX] = useState(0);
+  const [posY, setPosY] = useState(0);
+  const [coords, setCoords] = useState([]);
+
+  useEffect(() => {
+    const u = () => {
+      console.log("mouseup");
+      onDone({ coords, posX, posY });
+      setIsDrawing(false);
+    };
+    window.addEventListener("mouseup", u);
+    return () => {
+      window.removeEventListener("mouseup", u);
+    };
+  }, [coords, posX, posY]);
+
+  useEffect(() => {
+    const d = (e) => {
+      e.preventDefault();
+      console.log("mousedown", e);
+      setIsDrawing(true);
+      setPosX(e.pageX - DEFAULT_LENGTH / 2);
+      setPosY(e.pageY - DEFAULT_LENGTH / 2);
+      setCoords([[DEFAULT_LENGTH / 2, DEFAULT_LENGTH / 2]]);
+    };
+    window.addEventListener("mousedown", d);
+
+    const m = (e) => {
+      if (!isDrawing) {
+        return;
+      }
+      setCoords((c) => [...c, [e.pageX - posX, e.pageY - posY]]);
+    };
+    window.addEventListener("mousemove", m);
+
+    return () => {
+      window.removeEventListener("mousedown", d);
+      window.removeEventListener("mousemove", m);
+    };
+  }, [isDrawing, posX, posY]);
+
+  const svg = e(Layer, { coords, posX, posY });
 
   const overlay = e("div", { className: "draw-overlay" });
 
   return e(
     Fragment,
     {},
-    isOn && overlay,
+    overlay,
     e(
       "div",
       {
         className: "draw",
       },
-      !isOn && onBtn,
       svg
     )
+  );
+}
+
+function Annotate() {
+  const [layers, setLayers] = useState([]);
+  const [isOn, setIsOn] = useState(false);
+
+  const onBtn = e(
+    "button",
+    {
+      onClick: () => setIsOn((o) => !o),
+    },
+    isOn ? "OFF" : "ON"
+  );
+
+  // TODO: add id for key
+  const layerElements = layers.map((l) => e(Layer, l));
+
+  return e(
+    Fragment,
+    {},
+    e(
+      "div",
+      {
+        className: "annotate-toolbar",
+      },
+      onBtn
+    ),
+    isOn &&
+      e(DrawTool, {
+        onDone(layer) {
+          setLayers((l) => [...l, layer]);
+        },
+      }),
+    layerElements
   );
 }
 const domContainer = document.querySelector("#annotate");
